616667eb411193d5b4a6705ada608d37cbe4bda9
Ragil Alfarizi <ragilalfarizi23@gmail.com>
2024-10-15 10:22:06 +0700

    feat: add gps and rs485 tasks prototype

---
+++ include/common.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Arduino.h>
+
+// struct GPSData_s
+// {
+//     uint32_t lon_i_g;
+//     uint32_t lat_i_g; // integer global
+//     double lon_f_g; // float global
+//     double lat_f_g;
+//     bool lonValueNegative;
+//     bool latValueNegative;
+//     char GPSStatus;
+// };
+
+// New Way
+struct GPSData_s
+{
+    uint32_t lon_i_g;
+    uint32_t lat_i_g;
+    double lon_f_g;
+    double lat_f_g;
+    // double longitude;
+    // double latitude;
+    bool lonValueNegative;
+    bool latValueNegative;
+    char GPSStatus;
+};
+
+struct BeaconData_t {
+    GPSData_s gps;
+    float voltageSupply;
+    time_t hourMeter;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,35 @@
 #include "NimBLEBeacon.h"
 #include "NimBLEAdvertising.h"
 #include "NimBLEEddystoneURL.h"
+#include "common.h"
+#include "gps.h"
 
 /* DEKLARASI OBJEK YANG DIGUNAKAN TERSIMPAN DI HEAP */
 RTC *rtc;
 AnalogInput *ain;
+GPS *gps;
 
 /* FORWARD DECLARATION UNTUK FUNGSI-FUNGSI DI DEPAN*/
 static void RTCDemo(void *pvParam);
 static void analogDemo(void *pvParam);
 static void sendBLEData(void *pvParam);
+static void retrieveGPSData(void *pvParam);
+static void sendToRS485(void *pvParam);
 static void setCustomBeacon();
 
 TaskHandle_t RTCDemoHandler = NULL;
 TaskHandle_t analogDemoHandler = NULL;
 TaskHandle_t sendBLEDataHandler = NULL;
+TaskHandle_t retrieveGPSHandler = NULL;
+TaskHandle_t sendToRS485Handler = NULL;
 SemaphoreHandle_t xSemaphore = NULL;
 
 /* GLOBAL VARIABLES */
 BLEAdvertising *pAdvertising;
 float analogInputVal = 0;
+GPSData_s Internalgps;
+float voltageSupply = 0;
+time_t lastTenth = 0;
 
 void setup()
 {
@@ -41,6 +51,10 @@ void setup()
     Serial.println("Inisialisasi Analog Input");
     ain = new AnalogInput();
 
+    /* GPS INIT */
+    Serial.println("Inisialisasi GPS");
+    gps = new GPS();
+
     xSemaphore = xSemaphoreCreateBinary();
     xSemaphoreGive(xSemaphore);
 
@@ -53,6 +67,8 @@ void setup()
     xTaskCreatePinnedToCore(RTCDemo, "RTC Demo", 2048, NULL, 3, &RTCDemoHandler, 0);
     xTaskCreatePinnedToCore(analogDemo, "Analog Demo", 2048, NULL, 3, &analogDemoHandler, 0);
     xTaskCreatePinnedToCore(sendBLEData, "Send BLE Data", 2048, NULL, 3, &sendBLEDataHandler, 1);
+    xTaskCreatePinnedToCore(retrieveGPSData, "get GPS Data", 2048, NULL, 3, &retrieveGPSHandler, 0);
+    // xTaskCreatePinnedToCore(sendToRS485, "send data to RS485", 2048, NULL, 3, &sendToRS485Handler, 1);
 }
 
 void loop()
@@ -98,6 +114,8 @@ static void setCustomBeacon()
      * Total size: ~8 bytes (well within BLE’s advertisement limits).
      * */
 
+    char beacon_data[25];
+    uint16_t volt = voltageSupply * 1000; // 3300mV = 3.3V
     const uint16_t beaconUUID = 0xFEAA;
 
     // atur data advertising
@@ -107,24 +125,44 @@ static void setCustomBeacon()
     oScanResponseData.setFlags(0x06); // GENERAL_DISC_MODE 0x02 | BR_EDR_NOT_SUPPORTED 0x04
     oScanResponseData.setCompleteServices(BLEUUID(beaconUUID));
 
-    uint16_t voltage = random(2800, 3700); // dalam millivolts
-    analogInputVal = ain->readAnalogInput(AnalogPin::PIN_A1);
+    // uint16_t voltage = random(2800, 3700); // dalam millivolts
+    // analogInputVal = ain->readAnalogInput(AnalogPin::PIN_A1);
     // float current = 1.5;             // dalam ampere
     // uint32_t timestamp = 1678801234; // contoh Unix TimeStamp
 
     // Convert current to a 16-bit fixed-point format (e.g., 1.5 A -> 384 in 8.8 format)
     // Konversi arus ke format (contoh: 1.5 A -> 384 di format 8.8)
     // int16_t currentFixedPoint = (int16_t)(current * 256);
-    int16_t analogInputFixedPoint = (int16_t)(analogInputVal * 256);
+    // int16_t analogInputFixedPoint = (int16_t)(analogInputVal * 256);
 
-    char customData[4]; // 2 bytes untu voltage, 2 bytes untuk current, 4 bytes untuk timestamp
+    // char customData[4]; // 2 bytes untu voltage, 2 bytes untuk current, 4 bytes untuk timestamp
 
-    customData[0] = 0x20;
-    customData[1] = 0x00;
-    customData[2] = (analogInputFixedPoint >> 8) & 0xFF;
-    customData[3] = (analogInputFixedPoint & 0xFF);
+    // customData[0] = 0x20;
+    // customData[1] = 0x00;
+    // customData[2] = (analogInputFixedPoint >> 8) & 0xFF;
+    // customData[3] = (analogInputFixedPoint & 0xFF);
 
-    oScanResponseData.setServiceData(BLEUUID(beaconUUID), std::string(customData, sizeof(customData)));
+    beacon_data[0] = 0x01; // Eddystone Frame Type (Unencrypted Eddystone-TLM)
+    beacon_data[1] = 0x00; // TLM version
+    beacon_data[2] = 0x01;
+    beacon_data[3] = (volt >> 8);                                 // Battery voltage, 1 mV/bit i.e. 0xCE4 = 3300mV = 3.3V
+    beacon_data[4] = (volt & 0xFF);                               //
+    beacon_data[5] = 0;                                           // Eddystone Frame Type (Unencrypted Eddystone-TLM)
+    beacon_data[6] = Internalgps.GPSStatus;                       //
+    beacon_data[7] = ((Internalgps.lon_i_g & 0xFF000000) >> 24);  //
+    beacon_data[8] = ((Internalgps.lon_i_g & 0xFF0000) >> 16);    //
+    beacon_data[9] = ((Internalgps.lon_i_g & 0xFF00) >> 8);       //
+    beacon_data[10] = (Internalgps.lon_i_g & 0xFF);               //
+    beacon_data[11] = ((Internalgps.lat_i_g & 0xFF000000) >> 24); //
+    beacon_data[12] = ((Internalgps.lat_i_g & 0xFF0000) >> 16);   //
+    beacon_data[13] = ((Internalgps.lat_i_g & 0xFF00) >> 8);      //
+    beacon_data[14] = (Internalgps.lat_i_g & 0xFF);               //
+    beacon_data[15] = (((lastTenth / 10) & 0xFF000000) >> 24);    //
+    beacon_data[16] = (((lastTenth / 10) & 0xFF0000) >> 16);      //
+    beacon_data[17] = (((lastTenth / 10) & 0xFF00) >> 8);         //
+    beacon_data[18] = ((lastTenth / 10) & 0xFF);                  //
+
+    oScanResponseData.setServiceData(BLEUUID(beaconUUID), std::string(beacon_data, sizeof(beacon_data)));
     oAdvertisementData.setName("OMU Demo Data");
     pAdvertising->setAdvertisementData(oAdvertisementData);
     pAdvertising->setScanResponseData(oScanResponseData);
@@ -141,5 +179,44 @@ static void sendBLEData(void *pvParam)
         Serial.println("Advertising...");
         // pAdvertising->stop();
         vTaskDelay(pdMS_TO_TICKS(1000)); // advertising selama 3 detik
+    }
+}
+
+static void retrieveGPSData(void *pvParam)
+{
+    double longitude, latitude;
+    bool isValid = false;
+
+    while (1)
+    {
+        isValid = gps->getValidation();
+
+        if ((gps->getCharProcessed()) < 10)
+        {
+            Serial.println("GPS module not sending data, check wiring or module power");
+        }
+        else
+        {
+            if (isValid)
+            {
+
+                Serial.printf("Latitude : %f", latitude);
+                Serial.printf("Longitude : %f", longitude);
+            }
+            else
+            {
+                Serial.println("GPS is searching for a signal...");
+            }
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
+static void sendToRS485(void *pvParam)
+{
+    while (1)
+    {
+        // do something
     }
 }
