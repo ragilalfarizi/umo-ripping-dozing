02f8b3a7cf6c2f409c038bf65d6e0917ba3223bd
Ragil Alfarizi <ragilalfarizi23@gmail.com>
2024-10-16 16:47:30 +0700

    chore: clean up GPSData_s

--- include/common.h
+++ include/common.h
@@ -2,28 +2,11 @@
 
 #include <Arduino.h>
 
-// struct GPSData_s
-// {
-//     uint32_t lon_i_g;
-//     uint32_t lat_i_g; // integer global
-//     double lon_f_g; // float global
-//     double lat_f_g;
-//     bool lonValueNegative;
-//     bool latValueNegative;
-//     char GPSStatus;
-// };
-
 // New Way
 struct GPSData_s
 {
-    // uint32_t lon_i_g;
-    // uint32_t lat_i_g;
-    // double lon_f_g;
-    // double lat_f_g;
     double longitude;
     double latitude;
-    // bool lonValueNegative;
-    // bool latValueNegative;
     char status;
 };
 
