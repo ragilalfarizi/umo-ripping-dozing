5ec679bd5a5a1548ba284d3b3990caadc4dbdb6e
Ragil Alfarizi <ragilalfarizi23@gmail.com>
2024-10-31 11:57:26 +0700

    style: format rtc.cpp

--- lib/rtc/rtc.cpp
+++ lib/rtc/rtc.cpp
@@ -1,102 +1,94 @@
 #include "rtc.h"
 
-RTC::RTC()
-{
-    Serial.println("RTC Initialization");
+RTC::RTC() {
+  Serial.println("RTC Initialization");
 
-    Wire.begin();
+  Wire.begin();
 
-    if (!begin())
-    {
-        Serial.println("Couldn't find RTC");
-        Serial.flush();
-        while (1)
-            delay(10);
-    }
+  if (!begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
 
-    if (lostPower())
-    {
-        Serial.println("RTC lost power, let's set the time!");
-        // When time needs to be set on a new device, or after a power loss, the
-        // following line sets the RTC to the date & time this sketch was compiled
-        adjust(DateTime(F(__DATE__), F(__TIME__)));
-        // This line sets the RTC with an explicit date & time, for example to set
-        // January 21, 2014 at 3am you would call:
-        // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-    }
+  if (lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
 }
 
-RTC::~RTC()
-{
+RTC::~RTC() {}
+
+std::string RTC::getCurrentDateString(DateTime &time) {
+  std::string dateString, day, month, year;
+
+  day   = std::to_string(time.day());
+  month = std::to_string(time.month());
+  year  = std::to_string(time.year());
+
+  dateString = day + "/" + month + "/" + year;
+  return dateString;
 }
 
-std::string RTC::getCurrentDateString(DateTime &time)
-{
-    std::string dateString, day, month, year;
+std::string RTC::getCurrentTimeString(DateTime &time) {
+  std::string timeString, hour, minute;
 
-    day = std::to_string(time.day());
-    month = std::to_string(time.month());
-    year = std::to_string(time.year());
+  hour   = (time.hour() < 10 ? "0" : "") + std::to_string(time.hour());
+  minute = (time.minute() < 10 ? "0" : "") + std::to_string(time.minute());
 
-    dateString = day + "/" + month + "/" + year;
-    return dateString;
+  timeString = hour + ":" + minute;
+  return timeString;
 }
 
-std::string RTC::getCurrentTimeString(DateTime &time)
-{
-    std::string timeString, hour, minute;
+void RTC::printRTCData(DateTime &startTime) {
+  Serial.print(startTime.year(), DEC);
+  Serial.print('/');
+  Serial.print(startTime.month(), DEC);
+  Serial.print('/');
+  Serial.print(startTime.day(), DEC);
+  Serial.print(" (");
+  Serial.print(_daysOfTheWeek[startTime.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(startTime.hour(), DEC);
+  Serial.print(':');
+  Serial.print(startTime.minute(), DEC);
+  Serial.print(':');
+  Serial.print(startTime.second(), DEC);
+  Serial.println();
 
-    hour = std::to_string(time.hour());
-    minute = std::to_string(time.minute());
+  Serial.print(" since midnight 1/1/1970 = ");
+  Serial.print(startTime.unixtime());
+  Serial.print("s = ");
+  Serial.print(startTime.unixtime() / 86400L);
+  Serial.println("d");
 
-    timeString = hour + ":" + minute;
-    return timeString;
-}
+  // calculate a date which is 7 days, 12 hours, 30 minutes, 6 seconds into the
+  // future
+  DateTime future(startTime + TimeSpan(7, 12, 30, 6));
 
-void RTC::printRTCData(DateTime &startTime)
-{
-    Serial.print(startTime.year(), DEC);
-    Serial.print('/');
-    Serial.print(startTime.month(), DEC);
-    Serial.print('/');
-    Serial.print(startTime.day(), DEC);
-    Serial.print(" (");
-    Serial.print(_daysOfTheWeek[startTime.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(startTime.hour(), DEC);
-    Serial.print(':');
-    Serial.print(startTime.minute(), DEC);
-    Serial.print(':');
-    Serial.print(startTime.second(), DEC);
-    Serial.println();
+  Serial.print(" now + 7d + 12h + 30m + 6s: ");
+  Serial.print(future.year(), DEC);
+  Serial.print('/');
+  Serial.print(future.month(), DEC);
+  Serial.print('/');
+  Serial.print(future.day(), DEC);
+  Serial.print(' ');
+  Serial.print(future.hour(), DEC);
+  Serial.print(':');
+  Serial.print(future.minute(), DEC);
+  Serial.print(':');
+  Serial.print(future.second(), DEC);
+  Serial.println();
 
-    Serial.print(" since midnight 1/1/1970 = ");
-    Serial.print(startTime.unixtime());
-    Serial.print("s = ");
-    Serial.print(startTime.unixtime() / 86400L);
-    Serial.println("d");
+  Serial.print("Temperature: ");
+  Serial.print(getTemperature());
+  Serial.println(" C");
 
-    // calculate a date which is 7 days, 12 hours, 30 minutes, 6 seconds into the future
-    DateTime future(startTime + TimeSpan(7, 12, 30, 6));
-
-    Serial.print(" now + 7d + 12h + 30m + 6s: ");
-    Serial.print(future.year(), DEC);
-    Serial.print('/');
-    Serial.print(future.month(), DEC);
-    Serial.print('/');
-    Serial.print(future.day(), DEC);
-    Serial.print(' ');
-    Serial.print(future.hour(), DEC);
-    Serial.print(':');
-    Serial.print(future.minute(), DEC);
-    Serial.print(':');
-    Serial.print(future.second(), DEC);
-    Serial.println();
-
-    Serial.print("Temperature: ");
-    Serial.print(getTemperature());
-    Serial.println(" C");
-
-    // Serial.println();
-    delay(3000);
+  // Serial.println();
+  delay(3000);
 }
